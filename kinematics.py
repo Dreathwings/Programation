72bf8de2d9a7d7b8b75785101482419146c7ac8b
PC-Warren <warren.privat@ynov.com>
2021-11-02 16:24:44 +0100

    V0.3 OK+Sim 02/11-16:25

--- kinematics.py
+++ kinematics.py
@@ -2,33 +2,38 @@ import math
 from math import cos, pi,sin
 
 # Dimensions used for the PhantomX robot :
-constL1 = 54.8
-constL2 = 65.3
-constL3 = 133
-theta2Correction = 0  # A completer
-theta3Correction = 0  # A completer
+#constL1 = 54.8
+#constL2 = 65.3
+#constL3 = 133
+#theta2Correction = 0  # A completer
+#theta3Correction = 0  # A completer
 
 # Dimensions used for the simple arm simulation
-# bx = 0.07
-# bz = 0.25
-# constL1 = 0.085
-# constL2 = 0.185
-# constL3 = 0.250
+bx = 0.07
+bz = 0.25
+constL1 = 0.085
+constL2 = 0.185
+constL3 = 0.250
 
 
 def computeDK(theta1, theta2, theta3, l1=constL1, l2=constL2, l3=constL3):
-    # A completer
+    ''' Terminal
     theta1rad = (theta1*(pi/180))
     theta2rad = (theta2*(pi/180))
     theta3rad = (theta3*(pi/180))
 
-    x = ((l1+l2*cos(theta2rad)+l3*cos(theta2rad+theta3rad))*cos(theta1rad))
-    y = ((l1+l2*cos(theta2rad)+l3*cos(theta2rad+theta3rad))*sin(theta1rad))
-    z = (l2*sin(theta2rad)+l3*sin(theta2rad+theta3rad))
+    x = ((l1+l2*cos(theta2rad)+l3*cos(theta3rad+theta2rad))*cos(theta1rad))
+    y = ((l1+l2*cos(theta2rad)+l3*cos(theta3rad+theta2rad))*sin(theta1rad))
+    z = (l2*sin(theta2rad)+l3*sin(theta2rad+theta3rad))'''
+
+    '''Simulation'''
+    x = ((l1 + l2*cos(theta2) + l3*cos(theta3+theta2)) * cos(theta1))
+    y = ((l1 + l2*cos(theta2) + l3*cos(theta3+theta2)) * sin(theta1))
+    z = -(l2*sin(theta2) + l3*sin(theta2+theta3))
 
 
 
-    return [int(x), int(y), int(z)]
+    return [x, y, z]
 
 
 def computeIK(x, y, z, l1=constL1, l2=constL2, l3=constL3):
