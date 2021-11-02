c1a40b4741b3e1b4adde6f54f79ed997af91780c
PC-Warren <warren.privat@ynov.com>
2021-11-02 15:46:57 +0100

    V0.1

--- kinematics.py
+++ kinematics.py
@@ -1,5 +1,5 @@
 import math
-from math import cos,sin
+from math import cos, pi,sin
 
 # Dimensions used for the PhantomX robot :
 constL1 = 54.8
@@ -18,13 +18,17 @@ theta3Correction = 0  # A completer
 
 def computeDK(theta1, theta2, theta3, l1=constL1, l2=constL2, l3=constL3):
     # A completer
-    x = (l1+(l2*cos(theta2))+(l3*cos(theta2+theta3))cos(theta1))
-    y = (l1+(l2*cos(theta2))+(l3*cos(theta2+theta3))sin(theta1))
-    z = ((l2*sin(theta2))*(l3*sin(theta2+theta3)))
+    theta1rad = (theta1*(pi/180))
+    theta2rad = (theta2*(pi/180))
+    theta3rad = (theta3*(pi/180))
+
+    x = ((l1+l2*cos(theta2rad)+l3*cos(theta2rad+theta3rad))*cos(theta1rad))
+    y = ((l1+l2*cos(theta2rad)+l3*cos(theta2rad+theta3rad))*sin(theta1rad))
+    z = (l2*sin(theta2rad)+l3*sin(theta2rad+theta3rad))
 
 
 
-    return [x, y, z]
+    return [int(x), int(y), int(z)]
 
 
 def computeIK(x, y, z, l1=constL1, l2=constL2, l3=constL3):
@@ -37,9 +41,25 @@ def computeIK(x, y, z, l1=constL1, l2=constL2, l3=constL3):
 
 def main():
     print("Testing the kinematic funtions...")
+    print("L1 + L2 + L3= {}".format(
+        constL1+constL2+constL3
+    ) )
     print(
         "computeDK(0, 0, 0) = {}".format(
-            computeDK(0, 0, 0, l1=constL1, l2=constL2, l3=constL3)
+            computeDK(0, 0, 0, l1=constL1, l2=constL2, l3=constL3) # attendu: [253.1, 0, 0]
+
+        )
+    )
+    print(
+        "computeDK(90, 7, 11) = {}".format(
+            computeDK(90, 7, 11, l1=constL1, l2=constL2, l3=constL3)# attendu: [0, 246, 49]
+
+        )
+    )
+    print(
+        "computeDK(90, 88, 63) = {}".format(
+            computeDK(90, 88, 63, l1=constL1, l2=constL2, l3=constL3)# arrendu: [0, -59, 129]
+
         )
     )
 
